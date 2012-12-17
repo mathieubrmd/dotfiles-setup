344b38953f7b16cee5261ffbe89317f2686ac7d6
Ryan Albon <ryanalbon@gmail.com>
2012-12-17 03:06:40 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/ryanalbon/tmux-config.git ~/tmux-config
+git clone https://github.com/ryanalbon/vim-config.git ~/vim-config
+
+(cd ~/vim-config && git submodule init && git submodule update)
+
+ln -s ~/tmux-config/.tmux.conf ~/.tmux.conf
+ln -s ~/vim-config ~/.vim
+ln -s ~/.vim/.vimrc ~/.vimrc
